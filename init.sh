ba665995ac1d179bb0f97c677af0437da6fc8a72
sbarrios93 <sbarrios93@gmail.com>
2022-04-06 12:10:13 -0500

    added init shell

--- init.sh
+++ init.sh
@@ -2,6 +2,36 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
+# first thing is check that connecting to github with ssh works
+# if it doesn't, then we need to exit
+function github-authenticated() {
+    # Attempt to ssh to GitHub
+    ssh -T git@github.com &>/dev/null
+    RET=$?
+    if [[ $RET == 1 ]]; then
+        # user is authenticated, but fails to open a shell with GitHub
+        return 0
+    elif [[ $RET == 255 ]]; then
+        # user is not authenticated
+        return 1
+    else
+        echo "unknown exit code in attempt to ssh into git@github.com"
+    fi
+    return 2
+}
+
+if [[ $CI == 1 ]]; then
+    echo 'CI detected, skipping github-authenticated check'
+else
+    if github-authenticated; then
+        echo 'github-authenticated check passed'
+    else
+        echo 'github-authenticated check failed'
+        exit 1
+    fi
+fi
+
+# proceed with core setup
 # Install xcode
 if ! xcode-select -p >/dev/null; then
     echo "[localenv] Installing xcode"
@@ -22,6 +52,14 @@ fi
 if ! brew list --formula go-task >/dev/null 2>&1; then
     echo "[localenv] Installing go-task"
     brew install go-task/tap/go-task
+else
+    echo "[localenv] go-task is already installed with brew -> skipped"
+fi
+
+# Installing go-task
+if ! brew list --formula chezmoi >/dev/null 2>&1; then
+    echo "[localenv] Installing go-task"
+    brew install chezmoi
 else
     echo "[localenv] go-task is already installed with brew -> skipped"
 fi
