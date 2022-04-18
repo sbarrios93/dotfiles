86ca2f0c46dc711ab4bea507cc934dd6b10e63af
sbarrios93 <sbarrios93@gmail.com>
2022-04-17 22:04:25 -0500

    fix?

--- init.sh
+++ init.sh
@@ -25,6 +25,25 @@ if [[ "$OS" == "Linux" ]]; then
     export DEBIAN_FRONTEND=noninteractive
     sudo apt-get update
     sudo apt-get install git zsh -y
+    sudo apt install \
+        build-essential \
+        curl \
+        libbz2-dev \
+        libffi-dev \
+        liblzma-dev \
+        libncursesw5-dev \
+        libreadline-dev \
+        libsqlite3-dev \
+        python-openssl \
+        libssl-dev \
+        libxml2-dev \
+        libxmlsec1-dev \
+        llvm \
+        make \
+        tk-dev \
+        wget \
+        xz-utils \
+        zlib1g-dev -y
 fi
 
 # first thing is check that connecting to github with ssh works
