4131e9a6c0016e641929d2da8611fe805939650e
sbarrios93 <sbarrios93@gmail.com>
2022-04-29 20:15:06 -0500

    eval brew after installation

--- init.sh
+++ init.sh
@@ -112,6 +112,8 @@ fi
 if ! command -v brew >/dev/null 2>&1; then
     echo "${BLUE}[localenv] Installing brew${NC}"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "${BLUE}[localenv] brew is already installed -> skipped${NC}"
 fi
